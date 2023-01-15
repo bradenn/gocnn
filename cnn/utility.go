23935c29658ffd26425efc3e8368ceead189f462
Braden <braden@bradenn.com>
2023-01-15 15:08:41 -0800

    added flatten utility

--- cnn/utility.go
+++ cnn/utility.go
@@ -14,3 +14,21 @@ func populateMatUniform(dense *mat.Dense) {
 		}
 	}
 }
+
+// flatten takes in a matrix and returns a matrix with one row containing all data
+func flatten(input *mat.Dense) *mat.Dense {
+	// Get the dimensions of the input matrix
+	rows, cols := input.Dims()
+	// Compute the number of elements within the matrix
+	quanta := rows * cols
+	// Initialize an array to hold the values from the input
+	data := make([]float64, quanta)
+	// Pull the data from the input matrix and put it in the new array
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			data[i*cols+j] = input.At(i, j)
+		}
+	}
+	// return a new matrix with the flattened data
+	return mat.NewDense(1, quanta, data)
+}
